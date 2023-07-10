6989cbb9838cb8422b5b6511b54dc5b88bb75225
venkatasridhar <pvsridharroyal@gmail.com>
2023-07-10 14:45:44 +0530

    three

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in a new 
+          Learn React in a new variety of ways
+
+
 
         </a>
       </header>
