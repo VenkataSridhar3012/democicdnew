42721a55e47d2305cc54486fc3b75e0ee918d07f
venkatasridhar <pvsridharroyal@gmail.com>
2023-07-10 12:28:27 +0530

    second

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in a new new 
+
         </a>
       </header>
     </div>
